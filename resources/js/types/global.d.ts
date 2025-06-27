59266e7f3493186ca336d39bfce6eada80d9fe23
muhammadyudham <yudhay664@gmail.com>
2025-06-27 22:25:59 +0700

    Update global.d.ts

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,12 +1,24 @@
 import { AxiosInstance } from 'axios';
-import { route as ziggyRoute } from 'ziggy-js';
+import { Config, route as ziggyRoute, Router } from 'ziggy-js';
 
-declare module 'ziggy-js';
+declare module 'ziggy-js' {
+    interface ZiggyConfig extends Config {
+        // Jika Anda memiliki rute kustom atau bindings, definisikan di sini
+        // routes: {
+        //     'dashboard': [],
+        //     'profile.edit': [],
+        //     // ... dan seterusnya
+        // };
+    }
+}
 
 declare global {
     interface Window {
         axios: AxiosInstance;
+        // Ini adalah deklarasi global untuk objek Ziggy yang mungkin ada di Window
+        Ziggy: Config & { routes: { [key: string]: { uri: string, methods: string[], bindings?: { [key: string]: string }, parameters?: { [key: string]: string } } } };
     }
 
+    // Deklarasi fungsi `route` global yang bisa dipanggil
     var route: typeof ziggyRoute;
 }
